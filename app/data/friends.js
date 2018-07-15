80c2c02d279a74919813402f1f957c79947d8db8
Luz_Hernandez <hernandezluz18@gmail.com>
2018-07-15 16:30:52 -0400

    modified html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,10 @@
-var friends = [];
+var friends = [
+  {
+    name: "Luz",
+    photo:
+      "https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/15965866_10211975173237887_4963672767795924104_n.jpg?_nc_cat=0&oh=d12626b34468892bb6af6a7a812d6e52&oe=5BE3F36F",
+    scores: ["4", "3", "3", "3", "3", "5", "4", "4", "5", "3"]
+  }
+];
 
 module.exports = friends;
